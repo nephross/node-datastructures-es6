6bc3b042549854f3267c1ce855cefe03732b36bd
Peter Dalgaard-Jensen <nephross.cortex@gmail.com>
2018-01-31 16:01:01 +0100

    Fixing eslint and converting linkedList.ts to linkedList.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,18 @@
-'use strict';
-
-const chai = require('chai');
-const expect = chai.expect;
-const { describe, it, beforeEach, before, after } = require('mocha');
-
-// MODELS
-const model = require('./../../../api/lib/db/models');
-
-// CONTROLLER
-const mappingController = require('../../../api/controllers/mapping/mapping.controller');
-
-describe(__filename.replace(/\/.*\//, '').replace('.js', ''), () => {
-  beforeEach(async () => {
-    // EMPTY THE TABLES
-    await model.clients.sync({force: true});
-    await model.years.sync({force: true});
-    await model.mappingStatus.sync({force: true});
-  });
-
-  describe('Description of the test - another description change', () => {
-    it('Should pass because all is true - ish', async () => {
-      expect(true).to.eq(true);
-    });
-  });
-});
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": ["standard"],
+  "env": {
+    "browser": false,
+    "es6": true,
+    "node": true
+  },
+  "rules": {
+    "generator-star-spacing": ["error", {"before": true, "after": false}],
+    "semi": [2, "always"],
+    "space-before-function-paren": ["error", {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "always"
+    }]
+  }
+}
